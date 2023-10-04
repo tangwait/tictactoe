7e7af8423e6dfa7db4b86bb2fa9ff1fb48c5d534
tangwai <tmichael11314@gmail.com>
2023-10-04 00:43:23 -0500

    add gameBoard array add player constructor func

---
+++ script.js
@@ -0,0 +1,15 @@
+// store gameboard as an object >
+gameboard = [1, 2, 3,
+            4, 5, 6,
+            7, 8, 9];
+
+class player {
+    constructor(name, mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+}
+
+function createMark(mark) {
+
+}
