68e2b44862196043c84567d88da709a590528f4b
tangwai <tmichael11314@gmail.com>
2023-10-07 06:17:54 -0500

    add determineWinner() + code> >combination arrays/loop positions for 3in a row add updateDesc() to update text

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // store gameboard as an object >
 let mark = ''
+let currentPlayer = 'X';
 
 const displayBoard = () => {
     const game = document.querySelector('.game');
@@ -53,10 +54,48 @@ function selectMark() {
 selectMark();
 
 // player clicks x/o > store mark > put mark on page
-function createMark(mark) {
-    const boxes = document.querySelectorAll('.boxes')
+function createMark(event) {
+    const selectedBox = event.target;
+    const selectedBoxIndex = Array.from(selectedBox.parentNode.children).indexOf(selectedBox);
 
+    if (gameboard[selectedBoxIndex] === 'X' || gameboard[selectedBoxIndex] === 'O' || gameOver()) {
+        return;        
+    }
+    gameboard[selectedBoxIndex] = currentPlayer;
+    selectedBox.textContent = currentPlayer;
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function determineWinner() {
+    const winCombos = [
+        [1, 2, 3], [4, 5, 6], [7, 8, 9],
+        [1, 4, 7], [2, 5, 8], [3, 6, 9],
+        [1, 5, 9], [3, 5, 7]
+    ]; 
+
+    for (const combos of winCombos) {
+        const [a, b, c] = combos;
+        if (gameboard[a] && gameboard[a] === gameboard [b] && gameboard[a] === gameboard[c]) {
+            return gameboard[a];
+        }
+    }
+    return null;
+}
+
+function updateDesc(winningMark) {
+    const desc = document.querySelector('.desc');
+
+    if (winningMark) {
+        desc.textContent = `${winningMark} wins the game`
+    } else {
+        desc.textContent = "It's a draw!";
+    }
+}
+
+function gameOver() {
 
 }
 
 displayBoard();
+const winningMark = determineWinner();
+updateDesc(winningMark);
